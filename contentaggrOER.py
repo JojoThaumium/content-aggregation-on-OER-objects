a05d7b4e4d957ec25bc5ad762f98912deba8c2eb
Johann Schwarze <johann.schwarze.js@gmail.com>
2022-06-10 07:57:52 +0200

    Now takes URL from command-line arguments

--- contentaggrOER.py
+++ contentaggrOER.py
@@ -1,12 +1,16 @@
 import pikepdf
-#import sys
+import sys
 #import os
 import urllib.request
 from pathlib import Path
+
+#get the target pdf file from the command-line arguments
+pdf_url = sys.argv[1] #example https://bildungsportal.sachsen.de/opal/oer/8I6sM5zapD60
+
 #creates temp dir if it doesn't exist
 Path("temp").mkdir(parents=True, exist_ok=True)
 #downloads the file to temp dir and names it temp
-pdf_filename, headers = urllib.request.urlretrieve("https://bildungsportal.sachsen.de/opal/oer/8I6sM5zapD60", "temp/temp")
+pdf_filename, headers = urllib.request.urlretrieve(pdf_url, "temp/temp")
     # get the target pdf file from the command-line arguments
     #pdf_filename = sys.argv[1]
     #print(pdf_filename)
