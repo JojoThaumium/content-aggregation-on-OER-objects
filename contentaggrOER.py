db24149d8639c2a9aa0742e0344393e9e2e212e1
Johann Schwarze <johann.schwarze.js@gmail.com>
2022-06-10 07:51:19 +0200

    Now downloads to subdir temp and creates temp if it doesn't exist.

--- contentaggrOER.py
+++ contentaggrOER.py
@@ -2,11 +2,14 @@ import pikepdf
 #import sys
 #import os
 import urllib.request
-
-pdf_filename, headers = urllib.request.urlretrieve("https://bildungsportal.sachsen.de/opal/oer/1zfzKOhH3Szw")
+from pathlib import Path
+#creates temp dir if it doesn't exist
+Path("temp").mkdir(parents=True, exist_ok=True)
+#downloads the file to temp dir and names it temp
+pdf_filename, headers = urllib.request.urlretrieve("https://bildungsportal.sachsen.de/opal/oer/8I6sM5zapD60", "temp/temp")
     # get the target pdf file from the command-line arguments
     #pdf_filename = sys.argv[1]
-print(pdf_filename)
+    #print(pdf_filename)
 # read the pdf file
 pdf = pikepdf.Pdf.open(pdf_filename)
 docinfo = pdf.docinfo
